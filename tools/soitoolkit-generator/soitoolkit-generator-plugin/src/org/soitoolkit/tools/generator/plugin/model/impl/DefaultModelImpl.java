6522d0fed6565fcae01eff4181a4c27925f61123
Magnus Larsson <magnus.larsson.ml@gmail.com>
2010-11-12 19:09:21 +0000

    release v0.2.0

--- tools/soitoolkit-generator/soitoolkit-generator-plugin/src/org/soitoolkit/tools/generator/plugin/model/impl/DefaultModelImpl.java
+++ tools/soitoolkit-generator/soitoolkit-generator-plugin/src/org/soitoolkit/tools/generator/plugin/model/impl/DefaultModelImpl.java
@@ -39,7 +39,7 @@ import org.soitoolkit.tools.generator.plugin.util.XmlUtil;
 
 public class DefaultModelImpl implements IModel {
 
-	private static final String SOITOOLKIT_VERSION = "0.1.10-SNAPSHOT";
+	private static final String SOITOOLKIT_VERSION = "0.2.0";
 
 	private String groupId;
 	private String artifactId;
