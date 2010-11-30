ccbed10986f7cb538d370fc213cf60f256975f4d
Magnus Larsson <magnus.larsson.ml@gmail.com>
2010-11-30 07:45:27 +0000

    Update issue 45
    Checks ExceptionPayload instead of NullPayload.

--- commons/components/commons-mule/src/main/java/org/soitoolkit/commons/mule/soap/CreateSoapFaultIfExceptionTransformer.java
+++ commons/components/commons-mule/src/main/java/org/soitoolkit/commons/mule/soap/CreateSoapFaultIfExceptionTransformer.java
@@ -16,8 +16,6 @@
  */
 package org.soitoolkit.commons.mule.soap;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.text.MessageFormat;
 
 import org.mule.api.ExceptionPayload;
@@ -25,7 +23,6 @@ import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.api.transport.PropertyScope;
 import org.mule.transformer.AbstractMessageAwareTransformer;
-import org.mule.transport.NullPayload;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -45,60 +42,54 @@ public class CreateSoapFaultIfExceptionTransformer extends AbstractMessageAwareT
 		"  <faultcode>soap:Server</faultcode>\n" + 
 		"  <faultstring>{0}</faultstring>\n" +
 		"  <faultactor>{1}</faultactor>\n" +
-//		"  <detail>\n" +
-//		"    {2}\n" +
-//		"  </detail>\n" + 
+		"  <detail>\n" +
+		"    {2}\n" +
+		"  </detail>\n" + 
 		"</soap:Fault>";
     
     @Override
     public Object transform(MuleMessage message, String outputEncoding) throws TransformerException {
 
-    	System.err.println("CreateSoapFaultIfExceptionTransformer.transform() called");
-    	Object payload = message.getPayload();
+    	logger.debug("transform() called");
     		
 		// Take care of any error message and send it back as a SOAP Fault!
-    	// TODO: Do we need to actually check both NullPayload and presence of an ExceptionPayload???
-		if (payload instanceof NullPayload) {
+		// Is there an exception-payload?
+		ExceptionPayload ep = message.getExceptionPayload();
+        if (ep == null) {
+        	
+        	// No, it's no, just bail out returning what we got
+        	logger.debug("No error, return origin message");
+        	return message;
+        }
 
-			System.err.println("NullPayload detected, let's look for an ExceptionPayload as well...");
-			logger.debug("NullPayload detected, let's look for an ExceptionPayload as well...");
+		logger.debug("ExceptionPayload detected as well, let's create a SOAP-FAULT!");
+    	
+    	String soapFault = createSoapFaultFromExceptionPayload(ep);
+    	logger.debug("Created soapFault: {}", soapFault);
 
-			// We got a null-payload, let's see if there is an exception-payload instead...
-			ExceptionPayload ep = message.getExceptionPayload();
-	        if (ep != null) {
-
-				System.err.println("### ExceptionPayload detected as well, let's create a SOAP-FAULT!");
-				logger.debug("ExceptionPayload detected as well, let's create a SOAP-FAULT!");
-	        	
-	        	String soapFault = createSoapFaultFromExceptionPayload(ep);
-	        	
-				System.err.println("### Set ExceptionPayload to null and outbound http.status=500");
-				logger.debug("Set ExceptionPayload to null and outbound http.status=500");
-	            // Now the exception payload is transformed to a SOAP-Fault, remove the ExceptionPayload!
-	            message.setExceptionPayload(null);
-	        	message.setProperty("http.status", 500, PropertyScope.OUTBOUND);
-	            message.setPayload(soapFault);
-	            return message;
-	        }
-		}
+        // Now the exception payload is transformed to a SOAP-Fault, remove the ExceptionPayload!
+		logger.debug("Set ExceptionPayload to null and outbound http.status=500");
+        message.setExceptionPayload(null);
+    	message.setProperty("http.status", 500, PropertyScope.OUTBOUND);
+        message.setPayload(soapFault);
+        return message;
 	        
-    	System.err.println("No error, return origin message");
-		// No, no exception could be found, let's return the original MuleMessage
-		return message;
 	}
 
     protected String createSoapFaultFromExceptionPayload(ExceptionPayload ep) {
-        String errMsg   = ep.getCode() + ": " + ep.getMessage();
+    	
+    	// Use the root exception if any otherwise the exception
+		logger.debug("Exception: "     + ep.getException()     + ", " + ep.getException().getClass().getName());
+		logger.debug("RootException: " + ep.getRootException() + ", " + ep.getRootException().getClass().getName());
+    	Throwable e = (ep.getRootException() != null) ? ep.getRootException() : ep.getException();
+
+//		FIXME: Can't get soap fault over to the client!!!
+//		String errMsg   = ep.getCode() + ": " + ep.getMessage();
+        String errMsg   = e.getMessage();
         String endpoint = getEndpoint().getEndpointURI().getAddress();
-        String detail   = stackTraceToString(ep.getException());
+        String detail   = e.getMessage();
         return createSoapFault(errMsg, endpoint, detail);
 	}
-
-	protected String stackTraceToString(Throwable ex) {
-		StringWriter sw = new StringWriter();
-        ex.printStackTrace(new PrintWriter(sw));
-        return sw.toString();
-    }
 
 	protected String createSoapFault(String errMsg, String endpoint, String details) {
 		return MessageFormat.format(SOAP_FAULT_V11, errMsg, endpoint, details);
