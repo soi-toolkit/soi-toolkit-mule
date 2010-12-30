8863ee985ceb716c39adecdc0ba47da69279eb4b
Magnus Larsson <magnus.larsson.ml@gmail.com>
2010-12-30 10:09:55 +0000

    Corrected incorrect import

--- commons/components/commons-mule/src/test/java/org/soitoolkit/commons/mule/jaxb/JaxbUtilTest.java
+++ commons/components/commons-mule/src/test/java/org/soitoolkit/commons/mule/jaxb/JaxbUtilTest.java
@@ -25,7 +25,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.soitoolkit.commons.logentry.schema.v1.LogEntryType;
 import org.soitoolkit.commons.logentry.schema.v1.LogEvent;
-import org.soitoolkit.commons.mule.jdbc.JdbcUtil;
+import org.soitoolkit.commons.mule.jaxb.JaxbUtil;
 
 public class JaxbUtilTest {
 
