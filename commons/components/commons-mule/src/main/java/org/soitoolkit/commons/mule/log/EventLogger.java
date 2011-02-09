844e8a8e249fc334a2be60617b75d30d36e3c527
Magnus Larsson <magnus.larsson.ml@gmail.com>
2011-02-09 22:26:34 +0000

    Update issue 92
    * Corrected bug in EventLogger that prevented log messages to be produced
    
    * Corrected error handling for negative tests in oneway-generator for incorrect-input-message (returned exception from Mule changed in Mule 3)
    
    ISSUE: All negative tests written for Mule 2.2 will fail until corrected in the same way as above!

--- commons/components/commons-mule/src/main/java/org/soitoolkit/commons/mule/log/EventLogger.java
+++ commons/components/commons-mule/src/main/java/org/soitoolkit/commons/mule/log/EventLogger.java
@@ -142,8 +142,6 @@ public class EventLogger implements MuleContextAware {
 		String      logMessage,
 		Map<String, String> businessContextId,
 		Map<String, String> extraInfo) {
-
-// FIXME		System.err.println("### EventLogger.logInfoEvent(): messageLogger.isInfoEnabled() = " + messageLogger.isInfoEnabled());
 		
 		if (messageLogger.isInfoEnabled()) {
 			LogEvent logEvent = createLogEntry(LogLevelType.INFO, message, logMessage, businessContextId, extraInfo, message.getPayload(), null);
@@ -162,8 +160,6 @@ public class EventLogger implements MuleContextAware {
 		Map<String, String> businessContextId,
 		Map<String, String> extraInfo) {
 
-// FIXME		System.err.println("### EventLogger.logErrorEvent()");
-
 		LogEvent logEvent = createLogEntry(LogLevelType.ERROR, message, error.toString(), businessContextId, extraInfo, message.getPayload(), error);
 		
 		String logMsg = formatLogMessage(LOG_EVENT_ERROR, logEvent);
@@ -178,8 +174,6 @@ public class EventLogger implements MuleContextAware {
 		Object      payload,
 		Map<String, String> businessContextId,
 		Map<String, String> extraInfo) {
-
-// FIXME		System.err.println("### EventLogger.logErrorEvent()");
 
 		LogEvent logEvent = createLogEntry(LogLevelType.ERROR, null, error.toString(), businessContextId, extraInfo, payload, error);
 
@@ -276,7 +270,7 @@ public class EventLogger implements MuleContextAware {
 				stackTrace.append('\n').append("\t at ").append(stLine);
 			}
 		}
-		return MessageFormatter.arrayFormat(LOG_STRING, new String[] {logEventName, integrationScenarioId, contractId, logMessage, serviceImplementation, HOST_NAME, HOST_IP, componentId, endpoint, messageId, businessCorrelationId, businessContextIdString, extraInfoString, payload, stackTrace.toString(), logEventName}).toString();
+		return MessageFormatter.arrayFormat(LOG_STRING, new String[] {logEventName, integrationScenarioId, contractId, logMessage, serviceImplementation, HOST_NAME, HOST_IP, componentId, endpoint, messageId, businessCorrelationId, businessContextIdString, extraInfoString, payload, stackTrace.toString(), logEventName}).getMessage();
 	}
 	
 	private String businessContextIdToString(List<BusinessContextId> businessContextIds) {
