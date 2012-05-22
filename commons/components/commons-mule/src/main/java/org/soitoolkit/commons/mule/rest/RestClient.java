45ebda6e6fbe3e202a1637bec48e7bed1ff94426
Magnus Larsson <magnus.larsson.ml@gmail.com>
2012-05-22 12:08:34 +0000

    Update issue 256
    Improved the support for usage of two or more http-connectors in request/response flows.

--- commons/components/commons-mule/src/main/java/org/soitoolkit/commons/mule/rest/RestClient.java
+++ commons/components/commons-mule/src/main/java/org/soitoolkit/commons/mule/rest/RestClient.java
@@ -26,89 +26,177 @@ import org.mule.module.client.MuleClient;
 
 public class RestClient {
 	private MuleClient muleClient;
+	private String muleConnector;
+
+	public RestClient(MuleContext muleContext, String muleConnector) {
+		try {
+			muleClient = new MuleClient(muleContext);
+			this.muleConnector = muleConnector;
+		} catch (MuleException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	public RestClient(MuleContext muleContext) {
 		try {
 			muleClient = new MuleClient(muleContext);
+			this.muleConnector = null;
 		} catch (MuleException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
 	/**
-	 * Perform a HTTP POST call with json request
+	 * Perform a HTTP POST call with json request and utf8 charset
 	 * 
 	 * @param url
 	 * @param payload
 	 * @return
 	 * @throws MuleException
 	 */
-	public MuleMessage doHttpPostRequest_JsonContent(String url, String payload) throws MuleException {    	
+	public MuleMessage doHttpPostRequest_JsonContent(String url, String payload) {    	
 
-		Map<String, String> properties = new HashMap<String, String>();
-    	properties.put("http.method",    "POST");
-    	properties.put("Content-Type",   "application/json;charset=utf-8");
-
-    	MuleMessage response = muleClient.send(url, payload, properties);
-    	
-    	return response;
+		return doHttpSendRequest(url, "POST", payload, "application/json;charset=utf-8");
 	}
 	
 	/**
-	 * Perform a HTTP GET call with json response
-	 * 
-	 * @param url
-	 * @return
-	 * @throws MuleException
-	 */
-	public MuleMessage doHttpGetRequest_JsonConent(String url) throws MuleException {    	
-
-		Map<String, String> properties = new HashMap<String, String>();
-    	properties.put("http.method",    "GET");
-    	properties.put("Accept",         "application/json");
-    	properties.put("Accept-Charset", "utf-8");
-
-    	MuleMessage response = muleClient.send(url, null, properties);
-    	
-    	return response;
-	}
-
-	/**
-	 * Perform a HTTP PUT call with json request
+	 * Perform a HTTP POST call with xml request and utf8 charset
 	 * 
 	 * @param url
 	 * @param payload
 	 * @return
 	 * @throws MuleException
 	 */
-	public MuleMessage doHttpPutRequest_JsonContent(String url, String payload) throws MuleException {    	
+	public MuleMessage doHttpPostRequest_XmlContent(String url, String payload) {    	
 
-		Map<String, String> properties = new HashMap<String, String>();
-    	properties.put("http.method",    "PUT");
-    	properties.put("Content-Type",   "application/json;charset=utf-8");
-
-    	MuleMessage response = muleClient.send(url, payload, properties);
-    	
-    	return response;
+		return doHttpSendRequest(url, "POST", payload, "application/xml;charset=utf-8");
 	}
 	
 	/**
-	 * Perform a HTTP DELETE call with json response
+	 * Perform a HTTP GET call with json response and utf8 charset
 	 * 
 	 * @param url
 	 * @return
 	 * @throws MuleException
 	 */
-	public MuleMessage doHttpDeleteRequest_JsonConent(String url) throws MuleException {    	
+	public MuleMessage doHttpGetRequest_JsonContent(String url) {    	
+
+		return doHttpReceiveRequest(url, "GET", "application/json", "utf-8");
+	}
+
+	/**
+	 * Perform a HTTP GET call with xml response and utf8 charset
+	 * 
+	 * @param url
+	 * @return
+	 * @throws MuleException
+	 */
+	public MuleMessage doHttpGetRequest_XmlContent(String url) {    	
+
+		return doHttpReceiveRequest(url, "GET", "application/xml", "utf-8");
+	}
+
+	/**
+	 * Perform a HTTP PUT call with json request and utf8 charset
+	 * 
+	 * @param url
+	 * @param payload
+	 * @return
+	 * @throws MuleException
+	 */
+	public MuleMessage doHttpPutRequest_JsonContent(String url, String payload) {    	
+
+		return doHttpSendRequest(url, "PUT", payload, "application/json;charset=utf-8");
+	}
+
+	/**
+	 * Perform a HTTP PUT call with xml request and utf8 charset
+	 * 
+	 * @param url
+	 * @param payload
+	 * @return
+	 * @throws MuleException
+	 */
+	public MuleMessage doHttpPutRequest_XmlContent(String url, String payload) {    	
+
+		return doHttpSendRequest(url, "PUT", payload, "application/xml;charset=utf-8");
+	}
+
+	/**
+	 * Perform a HTTP DELETE call with json response and utf8 charset
+	 * 
+	 * @param url
+	 * @return
+	 * @throws MuleException
+	 */
+	public MuleMessage doHttpDeleteRequest_JsonContent(String url) {    	
+
+		return doHttpReceiveRequest(url, "DELETE", "application/json", "utf-8");
+	}
+
+	/**
+	 * Perform a HTTP DELETE call with xml response and utf8 charset
+	 * 
+	 * @param url
+	 * @return
+	 * @throws MuleException
+	 */
+	public MuleMessage doHttpDeleteRequest_XmlContent(String url) {    	
+
+		return doHttpReceiveRequest(url, "DELETE", "application/xml", "utf-8");
+	}
+	
+	/**
+	 * Perform a HTTP call sending information to the server using POST or PUT
+	 * 
+	 * @param url
+	 * @param method, e.g. "POST" or "PUT"
+	 * @param payload
+	 * @param contentType
+	 * @return
+	 * @throws MuleException
+	 */
+	public MuleMessage doHttpSendRequest(String url, String method, String payload, String contentType) {    	
 
 		Map<String, String> properties = new HashMap<String, String>();
-    	properties.put("http.method",    "DELETE");
-    	properties.put("Accept",         "application/json");
-    	properties.put("Accept-Charset", "utf-8");
+    	properties.put("http.method",    method);
+    	properties.put("Content-Type",   contentType);
 
-    	MuleMessage response = muleClient.send(url, null, properties);
+    	MuleMessage response = send(url, payload, properties);
+    	
+    	return response;
+	}
+	
+	/**
+	 * Perform a HTTP call receiving information from the server using GET or DELETE
+	 * 
+	 * @param url
+	 * @param method, e.g. "GET" or "DELETE"
+	 * @param acceptConentType
+	 * @param acceptCharSet
+	 * @return
+	 * @throws MuleException
+	 */
+	public MuleMessage doHttpReceiveRequest(String url, String method, String acceptConentType, String acceptCharSet) {    	
+
+		Map<String, String> properties = new HashMap<String, String>();
+    	properties.put("http.method",    method);
+    	properties.put("Accept",         acceptConentType);
+    	properties.put("Accept-Charset", acceptCharSet);
+
+    	MuleMessage response = send(url, null, properties);
     	
     	return response;
 	}
 
+	private MuleMessage send(String url, String payload, Map<String, String> properties) {
+		if (muleConnector != null) {
+			url += "?connector=" + muleConnector;
+		}
+		try {
+			return muleClient.send(url, payload, properties);
+		} catch (MuleException e) {
+			throw new RuntimeException(e);
+		}
+	}	
 }
