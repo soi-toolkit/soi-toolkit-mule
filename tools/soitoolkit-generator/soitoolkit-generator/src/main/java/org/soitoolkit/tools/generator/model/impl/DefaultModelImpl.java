2e3a782485e309eb25bdfe80fd5408ce27b31719
Magnus Larsson <magnus.larsson.ml@gmail.com>
2012-09-28 03:34:30 +0000

    Commit for releasing v0.6.0

--- tools/soitoolkit-generator/soitoolkit-generator/src/main/java/org/soitoolkit/tools/generator/model/impl/DefaultModelImpl.java
+++ tools/soitoolkit-generator/soitoolkit-generator/src/main/java/org/soitoolkit/tools/generator/model/impl/DefaultModelImpl.java
@@ -47,7 +47,7 @@ import org.soitoolkit.tools.generator.util.PreferencesUtil;
 
 public class DefaultModelImpl implements IModel {
 
-	private static final String SOITOOLKIT_VERSION = "0.5.1-SNAPSHOT";
+	private static final String SOITOOLKIT_VERSION = "0.6.0";
 
 	private static final String JMS_CONNECTOR     = "soitoolkit-jms-connector";
 	private static final String JMS_XA_CONNECTOR  = "soitoolkit-jms-xa-connector";
