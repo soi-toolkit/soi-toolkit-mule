ad83f316444552f74c2944c71d43904b3c385fac
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2014-11-29 17:24:46 +0100

    New Maven generator mojo's for the new V2 generators

---
+++ tools/soitoolkit-generator/soitoolkit-generator-maven-plugin/src/main/java/org/soitoolkit/tools/generator/maven/GenIntegrationComponentV2Mojo.java
@@ -0,0 +1,218 @@
+/* 
+ * Licensed to the soi-toolkit project under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The soi-toolkit project licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.soitoolkit.tools.generator.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.soitoolkit.tools.generator.Generator;
+import org.soitoolkit.tools.generator.IntegrationComponentV2Generator;
+import org.soitoolkit.tools.generator.model.ModelFactory;
+import org.soitoolkit.tools.generator.model.enums.DeploymentModelEnum;
+import org.soitoolkit.tools.generator.model.enums.MuleVersionEnum;
+import org.soitoolkit.tools.generator.model.enums.TransportEnum;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Goal for creating an new Integration Component
+ *
+ * @goal genICV2
+ * @requiresProject false
+ * 
+ * @author Magnus Larsson
+ */
+public class GenIntegrationComponentV2Mojo extends AbstractMojo {
+
+	/**
+     * ArtifactId.
+     * @parameter expression="${artifactId}" default-value="sample1"
+     * @required
+     */
+    private String artifactId;
+
+	/**
+     * GroupId.
+     * @parameter expression="${groupId}" default-value="org.sample"
+     * @required
+     */
+    private String groupId;
+
+	/**
+     * Version.
+     * @parameter expression="${version}" default-value="1.0.0-SNAPSHOT"
+     * @required
+     */
+    private String version;
+
+	/**
+     * Mule version.
+     * @parameter expression="${muleVersion}" default-value="3.5.0"
+     * @required
+     */
+    private String muleVersion;
+
+	/**
+     * Deploy model.
+     * @parameter expression="${deployModel}" default-value="Standalone"
+     * @required
+     */
+    private String deployModel;
+
+	/**
+     * Connectors.
+     * @parameter expression="${connectors}" default-value="JDBC,FTP,SFTP" // SERVLET
+     * @required
+     */
+    private String connectors;
+
+	/**
+     * Groovy Model.
+     * @parameter expression="${groovyModel}"
+     * @optional
+     */
+    private URL groovyModel;
+
+	/**
+     * Location of the output folder.
+     * @parameter expression="${outDir}" default-value="."
+     * @required
+     */
+    private File outDir;
+
+    private static String[] allowedConnectors = new String[] {"JDBC","FTP","SFTP","SERVLET"};
+    
+    public void execute() throws MojoExecutionException {
+
+    	
+        getLog().info("");
+        getLog().info("=========================================");
+        getLog().info("= Creating an new Integration Component =");
+        getLog().info("=========================================");
+        getLog().info("");
+        getLog().info("ARGUMENTS:");
+        getLog().info("(change an arg by suppling: -Darg=value):");
+        getLog().info("");
+        getLog().info("outDir=" + outDir.getPath());
+//        getLog().info("outDir=" + outDir.getAbsolutePath());
+//        try {
+//			getLog().info("outDir=" + outDir.getCanonicalPath());
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+        getLog().info("artifactId=" + artifactId);
+        getLog().info("groupId=" + groupId);
+        getLog().info("version=" + version);
+        getLog().info("muleVersion=" + muleVersion);
+        getLog().info("deployModel=" + deployModel);
+        getLog().info("connectors=JMS," + connectors);
+        getLog().info("groovyModel=" + groovyModel);
+        getLog().info("");
+
+        initGroovyModel();
+
+        MuleVersionEnum     muleVersionEnum = initMuleVersion(muleVersion);
+        DeploymentModelEnum deployModelEnum = initDeployModel(deployModel);
+        List<TransportEnum> connectorsEnum  = initConnectors(connectors);
+
+		Generator g = new IntegrationComponentV2Generator(System.out, groupId, artifactId, version, muleVersionEnum, deployModelEnum, connectorsEnum, outDir.getPath());
+
+		g.startGenerator();
+    }
+
+	private void initGroovyModel() throws MojoExecutionException {
+		if (groovyModel != null) {
+	    	try {
+				ModelFactory.setModelGroovyClass(groovyModel);
+			} catch (Exception e) {
+				throw new MojoExecutionException("Invalid Groovy model: " + groovyModel, e);
+			}
+		}
+	}
+
+	private MuleVersionEnum initMuleVersion(String muleVersion) throws MojoExecutionException {
+		MuleVersionEnum muleVersionEnum = null;
+		try {
+			muleVersionEnum = MuleVersionEnum.getByLabel(muleVersion);
+		} catch (Exception e) {
+			throw new MojoExecutionException("Invalid Mule version: " + muleVersion + ", allowed values: " + MuleVersionEnum.allowedLabelValues(), e);
+		}
+		if (muleVersionEnum == null) {
+			throw new MojoExecutionException("Invalid Mule version: " + muleVersion + ", allowed values: " + MuleVersionEnum.allowedLabelValues());
+		}
+		return muleVersionEnum;
+	}
+
+	private DeploymentModelEnum initDeployModel(String deployModel) throws MojoExecutionException {
+		DeploymentModelEnum deployModelEnum = null;
+		try {
+			deployModelEnum = DeploymentModelEnum.getByLabel(deployModel);
+		} catch (Exception e) {
+			throw new MojoExecutionException("Invalid deploy model: " + deployModel + ", allowed values: " + DeploymentModelEnum.allowedLabelValues(), e);
+		}
+		if (deployModelEnum == null) {
+			throw new MojoExecutionException("Invalid deploy model: " + deployModel + ", allowed values: " + DeploymentModelEnum.allowedLabelValues());
+		}
+		return deployModelEnum;
+	}
+
+	private List<TransportEnum> initConnectors(String connectors) throws MojoExecutionException {
+		List<TransportEnum> ts = new ArrayList<TransportEnum>();
+        ts.add(TransportEnum.JMS);
+
+        // Bail out if no extra connectors specified
+        if (connectors.trim().length() == 0) return ts;
+       
+
+        String[] connectorArr = connectors.split(",");
+		for (int i = 0; i < connectorArr.length; i++) {
+			connectorArr[i] = connectorArr[i].trim();
+		}
+
+        for (String connector : connectorArr) {
+        	TransportEnum t = getTransport(connector);
+        	ts.add(t);
+		}
+        
+        return ts;
+	}
+
+	private TransportEnum getTransport(String connector) throws MojoExecutionException {
+		TransportEnum transportEnum = null;
+
+		for (String allowedConnector : allowedConnectors) {
+			if (allowedConnector.equals(connector)) {
+				transportEnum = TransportEnum.valueOf(connector);
+			}
+		}
+		if (transportEnum == null) {
+			throw new MojoExecutionException("Invalid connector: " + connector + ", allowed values: " + getAllowedConnectors());
+		}
+		return transportEnum;
+	}
+	
+	private String getAllowedConnectors() {
+		String allowedConnectorsStr = "";
+		for (String allowedConnector : allowedConnectors) {
+			allowedConnectorsStr += allowedConnector + " ";
+		}
+	    return allowedConnectorsStr;
+	}
+}
---
+++ tools/soitoolkit-generator/soitoolkit-generator-maven-plugin/src/main/java/org/soitoolkit/tools/generator/maven/GenServiceV2Mojo.java
@@ -0,0 +1,242 @@
+/* 
+ * Licensed to the soi-toolkit project under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The soi-toolkit project licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.soitoolkit.tools.generator.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.soitoolkit.tools.generator.Generator;
+import org.soitoolkit.tools.generator.OnewayServiceV2Generator;
+import org.soitoolkit.tools.generator.RequestResponseServiceV2Generator;
+import org.soitoolkit.tools.generator.model.IModel;
+import org.soitoolkit.tools.generator.model.enums.MepEnum;
+import org.soitoolkit.tools.generator.model.enums.MuleVersionEnum;
+import org.soitoolkit.tools.generator.model.enums.TransformerEnum;
+import org.soitoolkit.tools.generator.model.enums.TransportEnum;
+import org.w3c.dom.Document;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static org.soitoolkit.commons.xml.XPathUtil.*;
+import static org.soitoolkit.tools.generator.Generator.GEN_METADATA_TYPE_KEY;
+import static org.soitoolkit.tools.generator.Generator.GEN_METADATA_TYPE_SERVICE;
+import static org.soitoolkit.tools.generator.util.PomUtil.extractGroupIdAndArtifactIdFromPom;
+
+/**
+ * Goal for creating an new Integration Component
+ *
+ * @goal genServiceV2
+ * @requiresProject false
+ * 
+ * @author Magnus Larsson
+ */
+public class GenServiceV2Mojo extends AbstractMojo {
+
+	/**
+     * Service.
+     * @parameter expression="${service}" default-value="sample"
+     * @required
+     */
+    private String service;
+
+	/**
+     * MessageExchangePattern.
+     * @parameter expression="${messageExchangePattern}" default-value="Request/Response"
+     * @required
+     */
+    private String messageExchangePattern;
+
+	/**
+     * InboundTransport.
+     * @parameter expression="${inboundTransport}" default-value="SOAPHTTP"
+     * @required
+     */
+    private String inboundTransport;
+
+	/**
+     * InboundTransport.
+     * @parameter expression="${outboundTransport}" default-value="SOAPHTTP"
+     * @required
+     */
+    private String outboundTransport;
+
+	/**
+     * Location of the output folder.
+     * @parameter expression="${outDir}" default-value="."
+     * @required
+     */
+    private File outDir;
+
+    private static String[] allowedRequestReplyInboundTransport  = new String[] {"SOAPHTTP","SOAPSERVLET", "RESTHTTP"};
+    private static String[] allowedRequestReplyOutboundTransport = new String[] {"SOAPHTTP","RESTHTTP","JMS"};
+    private static String[] allowedOneWayInboundTransport        = new String[] {"VM", "JMS", "JDBC", "FILE", "FTP", "SFTP", "HTTP", "SERVLET", "POP3", "IMAP"};
+    private static String[] allowedOneWayOutboundTransport       = new String[] {"VM", "JMS", "JDBC", "FILE", "FTP", "SFTP", "SMTP"};
+    private static String[] allowedOneWayRobustInboundTransport  = new String[] {"FILE", "FTP", "VM"};
+    private static String[] allowedOneWayRobustOutboundTransport = new String[] {"FILE", "FTP", "VM"};
+    
+    public void execute() throws MojoExecutionException {
+    	
+        getLog().info("");
+        getLog().info("==========================");
+        getLog().info("= Creating a new Service =");
+        getLog().info("==========================");
+        getLog().info("");
+        getLog().info("ARGUMENTS:");
+        getLog().info("(change an arg by suppling: -Darg=value):");
+        getLog().info("");
+        getLog().info("outDir=" + outDir.getPath());
+//        getLog().info("outDir=" + outDir.getAbsolutePath());
+//        try {
+//			getLog().info("outDir=" + outDir.getCanonicalPath());
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+        getLog().info("service=" + service);
+        getLog().info("messageExchangePattern=" + messageExchangePattern);
+        getLog().info("inboundTransport=" + inboundTransport);
+        getLog().info("outboundTransport=" + outboundTransport);
+        getLog().info("");
+
+        String pomFile = outDir.getPath() + "/pom.xml";
+
+        IModel m = initPomInfo(pomFile);
+
+        String artifactId = m.getArtifactId();
+        String groupId = m.getGroupId();
+
+        getLog().info("EXTRACTED POM-INFO:");
+        getLog().info("(from " + pomFile + ")");
+        getLog().info("artifactId=" + artifactId);
+        getLog().info("groupId=" + groupId);
+        getLog().info("");
+
+        MepEnum mepEnum = initMep(messageExchangePattern);
+        TransportEnum inboundTransportEnum  = initInboundTransport (mepEnum, inboundTransport);
+        TransportEnum outboundTransportEnum = initOutboundTransport(mepEnum, outboundTransport);
+
+        Generator g = null;
+        switch (mepEnum) {
+		case MEP_REQUEST_RESPONSE:
+			g = new RequestResponseServiceV2Generator(System.out, groupId, artifactId, service, MuleVersionEnum.MAIN_MULE_VERSION, inboundTransportEnum, outboundTransportEnum, TransformerEnum.JAVA, outDir.getPath());
+			break;
+
+		case MEP_ONE_WAY:
+			g = new OnewayServiceV2Generator(System.out, groupId, artifactId, service, MuleVersionEnum.MAIN_MULE_VERSION, inboundTransportEnum, outboundTransportEnum, TransformerEnum.JAVA, outDir.getPath());
+			break;
+
+		default:
+			throw new MojoExecutionException("Message Exchange Pattern [" + messageExchangePattern + "] not supported");
+		}
+		g.startGenerator();
+    }
+
+	private IModel initPomInfo(String pomFile) throws MojoExecutionException {
+		IModel m = null;
+		try {
+			InputStream pomIs = new FileInputStream(pomFile);
+			m = extractGroupIdAndArtifactIdFromPom(pomIs);
+
+			// Validate the presence of GENERATOR_TYPE_SERVICE in pom.xml
+			Document doc = createDocument(new FileInputStream(pomFile));
+			String docComment = getDocumentComment(doc);
+			String type = lookupParameterValue(GEN_METADATA_TYPE_KEY, docComment);
+			
+			if (!type.equals(GEN_METADATA_TYPE_SERVICE)) {
+				throw new MojoExecutionException("Invalid type of maven pom.xml-file (" + pomFile + "), missing the metadata: " +  GEN_METADATA_TYPE_KEY + "=" + GEN_METADATA_TYPE_SERVICE + ", found unexpected metadata: " +  GEN_METADATA_TYPE_KEY + "=" + type);
+			}
+			
+		} catch (FileNotFoundException e) {
+			throw new MojoExecutionException("Maven pom.xml-file not found at: " + pomFile, e);
+		}
+		return m;
+	}
+
+	private MepEnum initMep(String mep) throws MojoExecutionException {
+		MepEnum mepEnum = null;
+		try {
+			mepEnum = MepEnum.getByLabel(mep);
+		} catch (Exception e) {
+			throw new MojoExecutionException("Invalid Message Exchange Pattern: " + mep + ", allowed values: " + MepEnum.allowedLabelValues(), e);
+		}
+		if (mepEnum == null) {
+			throw new MojoExecutionException("Invalid Message Exchange Pattern: " + mep + ", allowed values: " + MepEnum.allowedLabelValues());
+		}
+		return mepEnum;
+	}
+
+	private TransportEnum initInboundTransport(MepEnum mepEnum, String inboundTransport2) throws MojoExecutionException {
+		String[] allowedTransports;
+		switch (mepEnum) {
+		case MEP_ONE_WAY:
+			allowedTransports = allowedOneWayInboundTransport;
+			break;
+		case MEP_ONE_WAY_ROBUST:
+			allowedTransports = allowedOneWayRobustInboundTransport;
+			break;
+		case MEP_REQUEST_RESPONSE:
+			allowedTransports = allowedRequestReplyInboundTransport;
+			break;
+		default:
+			throw new MojoExecutionException("MEP has no supported inbound transports: " + mepEnum);
+		}
+		return getTransport(inboundTransport, "inbound", allowedTransports);
+	}
+	
+	private TransportEnum initOutboundTransport(MepEnum mepEnum, String outboundTransport) throws MojoExecutionException {
+		String[] allowedTransports;
+		switch (mepEnum) {
+		case MEP_ONE_WAY:
+			allowedTransports = allowedOneWayOutboundTransport;
+			break;
+		case MEP_ONE_WAY_ROBUST:
+			allowedTransports = allowedOneWayRobustOutboundTransport;
+			break;
+		case MEP_REQUEST_RESPONSE:
+			allowedTransports = allowedRequestReplyOutboundTransport;
+			break;
+		default:
+			throw new MojoExecutionException("MEP has no supported outbound transports: " + mepEnum);
+		}
+		return getTransport(outboundTransport, "outbound", allowedTransports);
+	}
+
+	private TransportEnum getTransport(String transport, String direction, String[] allowedTransports) throws MojoExecutionException {
+		TransportEnum transportEnum = null;
+
+		for (String allowedTransport : allowedTransports) {
+			if (allowedTransport.equals(transport)) {
+				transportEnum = TransportEnum.valueOf(transport);
+			}
+		}
+		if (transportEnum == null) {
+			throw new MojoExecutionException("Invalid " + direction + " transport: " + transport + ", allowed values: " + getAllowedTransports(allowedTransports));
+		}
+		return transportEnum;
+	}
+	
+	private String getAllowedTransports(String[] allowedTransports) {
+		String allowedTransportsStr = "";
+		for (String allowedTransport : allowedTransports) {
+			allowedTransportsStr += allowedTransport + " ";
+		}
+	    return allowedTransportsStr;
+	}
+	
+}
